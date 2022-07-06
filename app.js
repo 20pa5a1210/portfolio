5d25a1d259a0cd2b5f8844d57662bb610b78724d
surya7826 <suryanarayana7826@gmail.com>
2022-07-06 19:00:37 +0530

    one

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path =  require('path')
+const app = express()
+
+app.use(express.static('./public'))
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./index/index.html'))
+})
+
+app.get('/home',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./index/index.html'))
+})
+
+app.get('/education',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./Education_page/education.html'))
+})
+
+app.listen(PORT,()=>{
+    console.log("Server Up and runnig");
+})
